d824694a334f1708c3b5e4bbc2b744756f2ab5d8
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-02 16:45:45 +0700

    Introduction Go - Operator

---
+++ konstanta.go
@@ -0,0 +1,23 @@
+// sesi constanta
+// const phi = 3.14
+// const aplikasi = "Belajar Golang"
+
+// kontsanta tanpa tipe
+const angka = 10 //Tipe akan disesuaikan
+
+// konstanta dengan tipe tertentu
+const umur int = 25
+
+const (
+	satu = 1
+	dua = 2
+	tiga = 3
+)
+
+// keywoard iota untuk membuat konstanta secara otomatis, biasanya digunakan dalam enum/deklarasi bertingkat (Untuk Nilai Berurutan)
+const (
+	A = iota // 0
+	B 		 // 1
+	C        // 2
+)
+
