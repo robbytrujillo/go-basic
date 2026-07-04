f2777160e45fe9a24a965435b9fcdc026085c889
robbytrujillo <robbyikusuma88@gmail.com>
2026-07-04 12:43:04 +0700

    Introduction Go - Pointer

--- pointer.go
+++ pointer.go
@@ -4,3 +4,36 @@ var ptr *int = &angka
 fmt.Println("Nilai angka:", angka)
 fmt.Println("Alamat angka:", &angka)
 fmt.Println("Pointer ptr:", ptr)
+
+// hasil
+fmt.Println("Nilai melalui pointer:", *ptr)
+
+// Pointer dan Fungsi
+// Tanpa pointer
+func ubah(x int) {
+    x = 100
+}
+
+func main() {
+    a := 10
+    ubah(a)
+    fmt.Println(a) // Tetap 10
+}
+
+// Dengan pointer
+func ubah(x *int) {
+    *x = 100
+}
+
+func main() {
+    a := 10
+    ubah(&a)
+    fmt.Println(a) // Jadi 100
+}
+
+// Pointer Default
+angka := new(int)
+*angka = 50
+fmt.Println(*angka) // Output: 50
+
+
